9b8458c284d719165bb6c7fe06c0b31f5be14952
林镇涛 <173039613@qq.com>
2020-10-23 09:05:22 +0800

    Adjust folder location
    Add MicroPython course and adjust the entire folder structure

--- Micropython/MicroPython_Codes/8.1_Serial_Print/8.1_Serial_Print.py
+++
@@ -1,8 +0,0 @@
-import time
-
-print("ESP32 initialization completed!")
-
-while True:
-    print("Running time : ", time.ticks_ms()/1000,"s")
-    time.sleep(1)
-
--- Micropython/MicroPython_Codes/8.2_Serial_Read_and_Write/8.2_Serial_Read_and_Write.py
+++
@@ -1,5 +0,0 @@
-print(str("\nESP32 initialization completed!\n")
-      + str("Please input some characters,\n")
-      + str("select \"Newline\" below and click send button. \n"))
-while True:
-    print("inputString: ",input())
