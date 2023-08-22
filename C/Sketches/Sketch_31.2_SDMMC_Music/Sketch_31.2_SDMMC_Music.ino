08b91fb8619e97d95fd782bd263fe7af318a0b9f
Zhentao-Lin <62634190+Zhentao-Lin@users.noreply.github.com>
2023-08-22 14:02:56 +0800

    Update tutorial.

---
+++ C/Sketches/Sketch_31.2_SDMMC_Music/Sketch_31.2_SDMMC_Music.ino
@@ -0,0 +1,99 @@
+/**********************************************************************
+  Filename    : SDMMC Music
+  Description : Play music from the sd card.
+  Auther      : www.freenove.com
+  Modification: 2023/08/21
+**********************************************************************/
+#include "Arduino.h"
+#include "Audio.h"
+#include "FS.h"
+#include "SD_MMC.h"
+
+#define SD_MMC_CMD 15
+#define SD_MMC_CLK 14
+#define SD_MMC_D0  2
+#define I2S_BCLK   26
+#define I2S_DOUT   22
+#define I2S_LRC    25
+
+Audio audio;
+
+void setup() {
+  Serial.begin(115200);
+  
+  SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+  if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD_MMC.cardType();
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD_MMC card attached");
+    return;
+  }
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+  uint64_t cardSize = SD_MMC.cardSize() / (1024 * 1024);
+  Serial.printf("SD_MMC Card Size: %lluMB\n", cardSize);
+
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  audio.setVolume(12);  // 0...21
+
+  audio.connecttoFS(SD_MMC, "/music/01.mp3");
+}
+
+void loop() {
+  audio.loop();
+  if (Serial.available()) {  // put streamURL in serial monitor
+    audio.stopSong();
+    String r = Serial.readString();
+    r.trim();
+    if (r.length() > 5) audio.connecttoFS(SD_MMC, r.c_str());
+    log_i("free heap=%i", ESP.getFreeHeap());
+  }
+}
+
+// optional
+void audio_info(const char *info) {
+  Serial.print("info        ");
+  Serial.println(info);
+}
+void audio_id3data(const char *info) {  //id3 metadata
+  Serial.print("id3data     ");
+  Serial.println(info);
+}
+void audio_eof_mp3(const char *info) {  //end of file
+  Serial.print("eof_mp3     ");
+  Serial.println(info);
+}
+void audio_showstation(const char *info) {
+  Serial.print("station     ");
+  Serial.println(info);
+}
+void audio_showstreamtitle(const char *info) {
+  Serial.print("streamtitle ");
+  Serial.println(info);
+}
+void audio_bitrate(const char *info) {
+  Serial.print("bitrate     ");
+  Serial.println(info);
+}
+void audio_commercial(const char *info) {  //duration in sec
+  Serial.print("commercial  ");
+  Serial.println(info);
+}
+void audio_icyurl(const char *info) {  //homepage
+  Serial.print("icyurl      ");
+  Serial.println(info);
+}
+void audio_lasthost(const char *info) {  //stream URL played
+  Serial.print("lasthost    ");
+  Serial.println(info);
+}
---
+++ C/Sketches/Sketch_34.1_CameraWebServer/Sketch_34.1_CameraWebServer.ino
@@ -0,0 +1,105 @@
+/**********************************************************************
+  Filename    : Camera Web Serrver
+  Description : ESP32 connects to WiFi and prints a url through a serial port.
+                Users visit the site to view the image data ESP32 camera.
+  Auther      : www.freenove.com
+  Modification: 2021/12/01
+**********************************************************************/
+#include "esp_camera.h"
+#include <WiFi.h>
+
+// ===================
+// Select camera model
+// ===================
+#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+// ** Espressif Internal Boards **
+//#define CAMERA_MODEL_ESP32_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
+//#define CAMERA_MODEL_ESP32S3_CAM_LCD
+
+
+#include "camera_pins.h"
+
+const char *ssid_Router     = "********";  //input your wifi name
+const char *password_Router = "********";  //input your wifi passwords
+camera_config_t config;
+
+void startCameraServer();
+void config_init();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  config_init();
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_vflip(s, 0);        //1-Upside down, 0-No operation
+  s->set_hmirror(s, 0);      //1-Reverse left and right, 0-No operation
+  s->set_brightness(s, 1);   //up the blightness just a bit
+  s->set_saturation(s, -1);  //lower the saturation
+
+  WiFi.begin(ssid_Router, password_Router);
+  while (WiFi.isConnected() != true) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  ;
+}
+
+void config_init() {
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_QVGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+}
