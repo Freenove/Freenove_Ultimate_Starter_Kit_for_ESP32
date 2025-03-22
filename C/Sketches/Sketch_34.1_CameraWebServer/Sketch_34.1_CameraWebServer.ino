3946d30df1358da9371a0a49942fb627cc32ca23
Zhentao-Lin <173039613@qq.com>
2025-03-22 11:37:00 +0800

    Update Sketch_34.1_CameraWebServer.ino

--- C/Sketches/Sketch_34.1_CameraWebServer/Sketch_34.1_CameraWebServer.ino
+++ C/Sketches/Sketch_34.1_CameraWebServer/Sketch_34.1_CameraWebServer.ino
@@ -95,7 +95,7 @@ void camera_init() {
   config.pin_sccb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
+  config.xclk_freq_hz = 10000000;
   config.frame_size = FRAMESIZE_QVGA;
   config.pixel_format = PIXFORMAT_JPEG; // for streaming
   //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
