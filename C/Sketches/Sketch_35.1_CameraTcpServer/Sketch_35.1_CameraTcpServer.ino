fb3a29b899aa6efa94a436b3824fd2c0b7e9631c
Zhentao-Lin <173039613@qq.com>
2025-03-22 11:37:08 +0800

    Update Sketch_35.1_CameraTcpServer.ino

--- C/Sketches/Sketch_35.1_CameraTcpServer/Sketch_35.1_CameraTcpServer.ino
+++ C/Sketches/Sketch_35.1_CameraTcpServer/Sketch_35.1_CameraTcpServer.ino
@@ -145,7 +145,7 @@ void cameraSetup() {
   config.pin_sccb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
+  config.xclk_freq_hz = 10000000;
   config.pixel_format = PIXFORMAT_JPEG;
 
   psramFound();
