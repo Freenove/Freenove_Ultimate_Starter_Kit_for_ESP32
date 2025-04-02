793b50be78a20c419ea78d2763546eb9cd0c9a77
Eeason Shen <109536807+Vegetable-SYC@users.noreply.github.com>
2025-04-02 10:34:56 +0800

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ os.system("rm -r freenove_Kit")
 os.system("git clone --depth 1 https://github.com/Freenove/Freenove_Ultimate_Starter_Kit_for_ESP32 freenove_Kit")
 
 project = "fnk0047-docs"
+# <!!!BEGIN!!!>
 copyright = '2016-2025, Freenove'
 author = 'Freenove'
 release = 'v1.0.0'
@@ -131,3 +132,4 @@ def setup(app):
     # app.add_css_file('https://cdn.jsdelivr.net/gh/Freenove/freenove-docs/docs/source/_static/css/custom.css')
 
 suppress_warnings = ['autosectionlabel.*']
+# <!!!END!!!>
