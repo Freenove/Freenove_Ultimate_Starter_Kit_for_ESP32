a68db88351039539d1396fc29351a467d8da7f3e
Vegetable-SYC <109536807+Vegetable-SYC@users.noreply.github.com>
2026-01-06 10:15:41 +0800

    update Bluetooth

---
+++ C/Sketches/Sketch_28.1_BluetoothByDAC(3.x.x)/Sketch_28.1_BluetoothByDAC(3.x.x).ino
@@ -0,0 +1,31 @@
+/*
+  Streaming data from Bluetooth to internal DAC of ESP32
+  
+  Copyright (C) 2020 Phil Schatzmann
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// ==> Example to use built in DAC of ESP32
+
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
+
+AnalogAudioStream out;
+BluetoothA2DPSink a2dp_sink(out);
+
+void setup() {
+  a2dp_sink.start("InternalDAC");  
+}
+
+
+void loop() {
+}
