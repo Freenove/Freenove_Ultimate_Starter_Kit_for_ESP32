7915e8a54583076a19799ad3fbfc2aec1e3d2fcd
Zhentao-Lin <173039613@qq.com>
2026-01-13 08:49:01 +0800

    Update Sketch_08.2_SerialRW.ino

--- C/Sketches/Sketch_08.2_SerialRW/Sketch_08.2_SerialRW.ino
+++ C/Sketches/Sketch_08.2_SerialRW/Sketch_08.2_SerialRW.ino
@@ -2,28 +2,33 @@
   Filename    : SerialRW
   Description : Use UART read and write data between ESP32 and PC.
   Auther      : www.freenove.com
-  Modification: 2024/06/18
+  Modification: 2026/01/12
 **********************************************************************/
-String inputString = "";      //a String to hold incoming data
+String inputString = "";      // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(String("\nESP32 initialization completed!\n")
-                + String("Please input some characters,\n")
-                + String("select \"Newline\" below and click send button. \n"));
+  Serial.println("\nESP32 initialization completed!");
+  Serial.println("Please input some characters,");
+  Serial.println("select \"Newline\" below and Ctrl + Enter to send message to ESP32.");
+  Serial.println("Ready to receive data...\n");
 }
 
 void loop() {
-  if (Serial.available()) {         // judge whether data has been received
-    char inChar = Serial.read();         // read one character
-    inputString += inChar;
-    if (inChar == '\n') {
+  if (Serial.available()) {
+    inputString = Serial.readStringUntil('\n');
+
+    // trim() removes leading and trailing whitespace characters, including \r, \n, tabs, etc.
+    inputString.trim();
+    
+    if (inputString.length() > 0) {
       stringComplete = true;
     }
   }
+
   if (stringComplete) {
-    Serial.printf("inputString: %s \n", inputString);
+    Serial.printf("inputString: %s\r\n", inputString.c_str());
     inputString = "";
     stringComplete = false;
   }
